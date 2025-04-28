6ea4b1bf28adb4e1dcdff7b55dfe0674fadafda1
omerm09 <omeremlik658@hotmail.com>
2025-04-28 20:21:42 +0300

    fourth

--- obj/Debug/net9.0/EgitimPlatformu.AssemblyInfo.cs
+++ obj/Debug/net9.0/EgitimPlatformu.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EgitimPlatformu")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63c80bc8e0e19612c3ef8cec8bdeaef954a754d8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02242216d2bab1421608442c0b33080c5d5115fe")]
 [assembly: System.Reflection.AssemblyProductAttribute("EgitimPlatformu")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EgitimPlatformu")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
